a65fb0ee486ddd55518c91a64c47d89413798bce
Jithin Palepu <44967770+jpalepu@users.noreply.github.com>
2023-12-07 08:51:19 +0100

    updated with certain time connection/disconnection

--- BLeTest.c
+++ BLeTest.c
@@ -14,23 +14,24 @@
 #include "services/gatt/ble_svc_gatt.h"
 #include "sdkconfig.h"
 #include "esp_bt.h"
-//#include "bletest.h"
 
-
-static const char *TAG = "BLE-Module";
-
-static int count;
+const char *TAG = "BLE-Server";
+uint8_t ble_addr;
+uint16_t notification_handle=0;
+uint16_t conn_handle;
+TimerHandle_t cnt_timer;
+int count, start_time, current_time;
 uint16_t waitTime; //variable to wait for 20 minutes to have some connection else Nimble stack shutdown
-static uint8_t ble_addr;
-static uint16_t conn_handle;
-static TimerHandle_t cnt_timer;
-static uint16_t notification_handle=0;
 
-static void cnt_reset(void);
-esp_err_t device_read(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg);
-esp_err_t device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg);
+
+bool conn_state         = false;
+bool conn_prev_state    = false;
+
+static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg);
+static int device_read(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg);
 static int ble_gap_event(struct ble_gap_event *event, void *arg); //this is just to notify an event, it can be connection or anything. 
 static int ble_app_advertise(void);
+static void cnt_reset(void);
 
 
 // BLE GATT service definition: write from the client to the server. 
@@ -40,12 +41,14 @@ static const struct ble_gatt_chr_def gatt_characteristics[] = {
         .flags = BLE_GATT_CHR_F_WRITE,
         .access_cb = device_write,
     },
+
     {
         .uuid = BLE_UUID16_DECLARE(0xEACD),
         .flags = BLE_GATT_CHR_F_READ | BLE_GATT_CHR_F_NOTIFY,
         .val_handle = &notification_handle,
         .access_cb = device_read,
     },
+
     {0}, // End of characteristics
 };
 
@@ -60,7 +63,6 @@ static const struct ble_gatt_svc_def gatt_svcs[] = {
 }; 
 
 static void notify_device(TimerHandle_t ev){
-    
     struct os_mbuf *om;
     int rc;
     count++;
@@ -92,18 +94,20 @@ static void cnt_reset(void)
 }
 
 // Write data to ESP32 defined as server
-esp_err_t device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
+static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
 {
     printf("Data from the client: %.*s\n", ctxt->om->om_len, ctxt->om->om_data);
-    return ESP_OK;
+    return 0;
 }
 
 // Read data from ESP32 defined as server
-esp_err_t device_read(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
+static int device_read(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
 {
     os_mbuf_append(ctxt->om, "test send/read from server", strlen("send/read test server"));
-    return ESP_OK;
+    return 0;
 }
+
+
 
 // BLE event handling
 static int ble_gap_event(struct ble_gap_event *event, void *arg) 
@@ -112,11 +116,17 @@ static int ble_gap_event(struct ble_gap_event *event, void *arg)
     {
     case BLE_GAP_EVENT_CONNECT:
         ESP_LOGI(TAG, "BLE GAP EVENT CONNECT %s", event->connect.status == 0 ? "OK!" : "FAILED!");
-        if (event->connect.status != 0)
-            ble_app_advertise();
+        if (event->connect.status == 0) {
+            conn_state = true;
+        }
+        break;
+    case BLE_GAP_EVENT_DISCONNECT:
+        ESP_LOGI(TAG, "BLE GAB DISCONNECT REASON: %d", event->disconnect.reason);
+        ble_app_advertise();
+        conn_state = false;
         break;
     case BLE_GAP_EVENT_ADV_COMPLETE: //complete discovery procedure
-        ESP_LOGI(TAG, "BLE GAP EVENT");
+        ESP_LOGI(TAG, "BLE GAP EVENT FINISHED...");
         ble_app_advertise();
         break;
     case BLE_GAP_EVENT_SUBSCRIBE:
@@ -128,8 +138,9 @@ static int ble_gap_event(struct ble_gap_event *event, void *arg)
     return 0;
 }
 
+
 // Define the BLE connection, then use the field to adverstise the data. this is more like structuring the packet to adverstise
-static int ble_app_advertise(void)
+ int ble_app_advertise(void)
 {
     struct ble_hs_adv_fields fields;
     const char *device_name;
@@ -146,40 +157,33 @@ static int ble_app_advertise(void)
     adv_params.conn_mode = BLE_GAP_CONN_MODE_UND; //undirected connection mode to accept req from any peer device. 
     adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;  //this means it is general discoverable (a peripheral device can be seen by a central device as long as it is advertising)
 
+    ESP_LOGI(TAG, " ADVERTISING...");
     uint8_t ret = ble_gap_adv_start(ble_addr, NULL, BLE_HS_FOREVER, &adv_params, ble_gap_event, NULL);
-    
     return ret;
 
 }
 
-esp_err_t ble_deinit(){
-   
-    ESP_LOGI(TAG, "Ble Stack is shutting down as there is no conn!");
-    nvs_flash_deinit();
-    esp_nimble_hci_deinit();
-    nimble_port_deinit();
-    //ble_hs_shutdown(BLE_GAP_EVENT_CONNECT);
-    //esp_bt_sleep_enable();
-    return ESP_OK;
+void ble_stop_advertise(){
+
+    ble_gap_adv_stop();
 
 }
 
 // BLE application synchronization callback
-esp_err_t ble_app_on_sync(void)
+static void ble_app_on_sync(void)
 {
+   //struct ble_gap_event *event; && event->connect.status != 0
     ble_hs_id_infer_auto(0, &ble_addr);
+    start_time = esp_timer_get_time();
     ble_app_advertise();
- 
-    return ESP_OK;  
 }
-
 // Host task
 static void host_task(void *param)
 {
     nimble_port_run();
 }
-/*
-esp_err_t ble_init(){
+
+void ble_init(){
 
     nvs_flash_init();
     esp_nimble_hci_init();
@@ -187,42 +191,69 @@ esp_err_t ble_init(){
     ble_svc_gap_init();
     ble_svc_gatt_init();
     ble_svc_gap_device_name_set("BLE-Server");
+    ble_gatts_count_cfg(gatt_svcs);
+    ble_gatts_add_svcs(gatt_svcs);
+    ble_hs_cfg.sync_cb = ble_app_on_sync;
+    nimble_port_freertos_init(host_task);
 
-    return ESP_OK;
 }
 
-//initialization of the hardware inside. 
-void app_main()
-{   
+void ble_deinit(){
+
+    ESP_LOGI(TAG, "DEINITIALIZING BLE...");
+    nimble_port_deinit();
+    esp_nimble_hci_deinit();
+    nvs_flash_deinit();
+
+}
+
+// initialization of the hardware inside. 
+// void app_main()
+// {   
+//     ble_init();
+//     cnt_timer = xTimerCreate("count timer", pdMS_TO_TICKS(20000), pdTRUE, 0, notify_device);
+//     cnt_reset();
+//     vTaskDelay(pdMS_TO_TICKS(20000));
+//     if(esp_timer_get_time() < 500 * 1000 * 1000){
+//     ble_app_advertise();
+//     }
+
+// }
+
+void app_main(){
+
+    uint64_t time = 65 * 1000 * 1000;
+    uint8_t ret;
     ble_init();
-    cnt_timer = xTimerCreate("count timer", pdMS_TO_TICKS(25000), pdTRUE, 0, notify_device);
-    cnt_reset();
-    ble_gatts_count_cfg(gatt_svcs);
-    ble_gatts_add_svcs(gatt_svcs);
-    ble_hs_cfg.sync_cb = ble_app_on_sync;
-    nimble_port_freertos_init(host_task);
-}*/
 
-
-
-esp_err_t ble_init(){
+    ESP_LOGI(TAG, "Initialized the Ble-Server");
     
-    nvs_flash_init();
-    esp_nimble_hci_init();
-    nimble_port_init();
-    ble_svc_gap_init();
-    ble_svc_gatt_init();
-    ble_svc_gap_device_name_set("BLE-Server");
-    cnt_timer = xTimerCreate("count timer", pdMS_TO_TICKS(25000), pdTRUE, 0, notify_device);
-    cnt_reset();
-    ble_gatts_count_cfg(gatt_svcs);
-    ble_gatts_add_svcs(gatt_svcs);
-    ble_hs_cfg.sync_cb = ble_app_on_sync;
-    nimble_port_freertos_init(host_task);
-
-    return ESP_OK;
-
+    // cnt_timer = xTimerCreate("count timer", pdMS_TO_TICKS(20000), pdTRUE, 0, notify_device);
+    // cnt_reset();
+    
+    // while(esp_timer_get_time() < time){
+    current_time = esp_timer_get_time();
+    //(start_time - current_time) > 10000
+    while((start_time - current_time) < 100000){
+        
+        vTaskDelay(pdMS_TO_TICKS(100));
+    
+        
+        if(conn_state && conn_state != conn_prev_state) {
+            conn_prev_state = conn_state;
+            ESP_LOGI(TAG, "Connection Made!");
+        }
+        
+        if(!conn_state && conn_state != conn_prev_state){
+            conn_prev_state = conn_state;
+            ESP_LOGI(TAG, "Disconnected!");
+        }
+    }
+    ESP_LOGI(TAG, "Stopped Advertising..");
 }
+
+
+
 
 
 
